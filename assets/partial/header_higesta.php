816e4f1720ec2baa0fc994717c01ae8a503eb6bb
磯龍矢 <isoryuya@isoryuuyanoMacBook-Pro.local>
2017-10-24 21:26:20 +0800

    ファビコン

---
+++ assets/partial/header_higesta.php
@@ -0,0 +1,27 @@
+<?php 
+$domain = "http://higesta.com/" ;
+?>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+
+
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+<link href="<?php echo $domain ; ?>assets/css/toolkit.css" rel="stylesheet">
+<link href="<?php echo $domain ; ?>assets/css/application.css" rel="stylesheet">
+
+<!-- ファビコン -->
+<link rel="shortcut icon" href="">
+
+<style>
+body {
+	width: 1px;
+	min-width: 100%;
+	*width: 100%;
+}
+</style>
--- index.php
+++ index.php
@@ -7,30 +7,10 @@ require_once($_SERVER["DOCUMENT_ROOT"]."assets/common/session_check.php") ;
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="keywords" content="">
-  <meta name="author" content="">
-
   <title>
     Home &middot; 
   </title>
-
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
-  <link href="assets/css/toolkit.css" rel="stylesheet">
-  <link href="assets/css/application.css" rel="stylesheet">
-  <style>
-  /* note: this is a hack for ios iframe for bootstrap themes shopify page */
-  /* this chunk of css is not part of the toolkit :) */
-  body {
-    width: 1px;
-    min-width: 100%;
-    *width: 100%;
-  }
-</style>
-
+  <?php include($_SERVER["DOCUMENT_ROOT"]."assets/partial/header_higesta.php") ; ?>
 </head>
 
 <body class="with-top-navbar">
@@ -73,6 +53,6 @@ require_once($_SERVER["DOCUMENT_ROOT"]."assets/common/session_check.php") ;
 
       <?php include($_SERVER["DOCUMENT_ROOT"]."assets/partial/read_js.php") ; ?>
 
-</body>
-</html>
+    </body>
+    </html>
 
